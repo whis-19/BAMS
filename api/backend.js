571409ba8a64eefc90c9efa7a0e9bd8c6d19f832
Muhammad Abdullah <132306415+whis-19@users.noreply.github.com>
2025-11-17 10:58:03 +0500

    Vercel Test

--- api/backend.js
+++ api/backend.js
@@ -1,6 +1,6 @@
 // Shim for Vercel: expose the Express app from backend as a serverless function
 // Vercel will detect files under /api as serverless functions.
 
-const app = require('../backend/index');
-
+const app = require("../backend/index.js");
 module.exports = app;
+
